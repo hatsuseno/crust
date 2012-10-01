1c58d15c5f4096c391eb7c3abdcda9082ffc8c50
Rudolf Leermakers <rudolf@hatsuseno.org>
2012-10-01 21:51:58 +0200

    Adding simple toolbar buttons for saving the grid's store / removing elements (doesn't work due to bug in store definition?)

--- public/scripts/crust/view/Resource.js
+++ public/scripts/crust/view/Resource.js
@@ -1,6 +1,23 @@
 Ext.define('Crust.view.Resource', {
     extend: 'Ext.grid.Panel',
 
+    tbar: [{
+        xtype: 'button',
+        text: 'Save changes',
+        handler: function() {
+            this.up('grid').getStore().commitChanges();
+        }
+    }, {
+        xtype: 'button',
+        text: 'Delete item',
+        handler: function() {
+            var grid = this.up('grid');
+
+            grid.getStore().remove(grid.getSelectionModel().getSelection());
+            grid.getStore().commitChanges();
+        }
+    }],
+
     listeners: {
         itemdblclick: function(panel, record) {
         }
