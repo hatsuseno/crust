966bf76c4fcd5e3a5bd613bbed63c2be1fa76c76
Rudolf Leermakers <rudolf@hatsuseno.org>
2012-10-01 22:11:04 +0200

    Adding some comments

--- lib/Rest/Dispatch.php
+++ lib/Rest/Dispatch.php
@@ -7,6 +7,10 @@ use CWX\Crust\Core\Dispatch as BaseDispatch,
     CWX\Crust\Db\Table,
     CWX\Crust\Request\Request;
 
+// Ye olde database discovery mechanism
+// This extends Dispatch to become a class capable
+// of generating RequestHandlers for stuff in the
+// database that we want to publicize
 class Dispatch extends BaseDispatch {
     protected $manager;
 
@@ -16,13 +20,22 @@ class Dispatch extends BaseDispatch {
         $tables = $manager->getTables();
         $pool = $this->getHandlerPool();
 
+        // The index handler provides a meta-resource, allowing
+        // reflection on the tables as resources to be inspected
         $indexHandler = new IndexHandler();
         $indexHandler->setDbManager($manager);
 
         $pool->add($indexHandler);
 
         $tables->map(function (Table $table) use ($pool) {
+            // EntityHandler is of course the 'star of the show'
+            // and allows GET/PUT/DELETE access to our entities
             $handler = new EntityHandler();
+
+            // SetHandler deals with things not addressed to a single
+            // entity, but a set of them. Adds reflection of table
+            // content through a GET and allows the addition of new
+            // entities to the set through POST
             $index = new SetHandler();
 
             $handler->setTable($table);
