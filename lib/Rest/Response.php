4f95b1da35431b6ecbdbdd689f80744bc16e9ba0
Rudolf Leermakers <rudolf@hatsuseno.org>
2012-10-01 22:19:50 +0200

    Adding some convenience methods to the basic REST Response class

--- lib/Rest/Response.php
+++ lib/Rest/Response.php
@@ -8,4 +8,23 @@ class Response extends BaseResponse {
     public function init() {
         $this->setHeader('Content-Type', 'application/json');
     }
+
+    public function setEntity($entity) {
+        return $this->setContent(json_encode(array(
+            'entity' => $entity
+        )));
+    }
+
+    public function setCollection(array $entities) {
+        return $this->setContent(json_encode(array(
+            'entities' => $entities
+        )));
+    }
+
+    public function setOK($message = '') {
+        return $this->setContent(json_encode(array(
+            'success' => true,
+            'message' => $message
+        )));
+    }
 }
